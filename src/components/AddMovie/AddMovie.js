d854ada47224569d5ea70f6ed379c7a4360ce26b
Justus Witmer <justus.witmer@gmail.com>
2020-10-03 08:41:58 -0500

    dropdown is generated by GET genre route

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 class AddMovie extends Component {
 
-  selectGenre = () => {
+  componentDidMount = () => {
     console.log('in selectGenre');
     this.props.dispatch({
       type: 'FETCH_GENRES'
@@ -27,32 +27,17 @@ class AddMovie extends Component {
           type='text'
           placeholder='description'
         />
-        <select onSelect={this.selectGenre}>
-          <option value=''></option>
-          <option value='adventure'>Adventure</option>
-          <option value='animated'>Animated</option>
-          <option value='biographical'>Biographical</option>
-          <option value='comedy'>Comedy</option>
-          <option value='disaster'>Disaster</option>
-          <option value='drama'>Drama</option>
-          <option value='epic'>Epic</option>
-          <option value='fantasy'>Fantasy</option>
-          <option value='musical'>Musical</option>
-          <option value='romantic'>Romantic</option>
-          <option value='scienceFiction'>Science Fiction</option>
-          <option value='space-opera'>Space-Opera</option>
-          <option value='superhero'>Superhero</option>
+        <select>
+          {this.props.genres.map(genre =>
+            <option key={genre.id} value={genre.id}>{genre.name}</option>
+          )}
+
         </select>
         <button
           onClick={() => this.props.history.push('/')}
         >Cancel
         </button>
         <button>Save</button>
-
-        <button onClick={this.selectGenre}>Genre</button>
-        {this.props.genres.map(genre =>
-          <p>{genre.name}</p>
-        )}
       </div>
     )
   }
